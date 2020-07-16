6bc8b8b558df6520ab55acecefa6e502653e76b8
mr-pramod-98 <mr.pj2k18@gmail.com>
2020-07-16 20:04:34 +0530

    Added "skills, certifications and education" pages

--- portfolio/content/urls.py
+++ portfolio/content/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('skills/', views.skills, name="skills"),
+    path('certifications/', views.certifications, name="certifications"),
+    path('education/', views.education, name="education")
 ]
--- portfolio/content/views.py
+++ portfolio/content/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def skills(request):
+    return render(request, 'skills.html')
+
+
+def certifications(request):
+    return render(request, 'certifications.html')
+
+
+def education(request):
+    return render(request, 'education.html')
