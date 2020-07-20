6ab8b3ac7501c889cc9395eaf3aa1cb49ef86b6e
mr-pramod-98 <mr.pj2k18@gmail.com>
2020-07-21 00:02:12 +0530

    Small design changes

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'hl&tb-384w!n^j#=tb3p&3r5oks7umtv9rcmd1m2esq!37)e1b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
